9a225325d5e4d002a4d988d6f26a18635f78d868
musdrop <2712448668@qq.com>
2024-12-16 19:42:59 +0800

    仓库地址修改

--- myserver.sh
+++ myserver.sh
@@ -1,7 +1,7 @@
 # 拉取配置仓库
-git clone github.com/musdrop/musdrop_conf ~/musdrop_conf
+git clone https://github.com/musdrop/musdrop_confs.git ~/musdrop_confs
 # 进入仓库目录
-cd ~/musdrop_conf
+cd ~/musdrop_confs
 # 运行 nps
 docker run -d --name=nps --restart=always -p 8001:8080 -p 8002:80 -p 8003:443 -p 8004:8024 -v ./nps:/conf musdrop/nps:v2
 # 安装 nginx
